27652148e0f75564f80e9271da75decad3228f0d
Sheld5 <soldasim@fel.cvut.cz>
2025-09-20 18:15:23 +0200

    update cluster script

--- cluster_scripts/queue_jobs.jl
+++ cluster_scripts/queue_jobs.jl
@@ -2,16 +2,21 @@
 
 include("../src/main.jl")
 
-function queue_jobs(problem::AbstractProblem, run_name::String)
+function queue_jobs(problem::AbstractProblem, run_name::String; selected_runs=nothing)
     problem_name = string(typeof(problem))
 
     start_files = Glob.glob(starts_dir(problem) * "/start_*.jld2")
-    @info "Running $(length(start_files)) runs of the $(typeof(problem)) ..."
-    
+    n_runs = isnothing(selected_runs) ? length(start_files) : length(selected_runs)
+    @info "Running $(n_runs) runs of the $(typeof(problem)) ..."
+
     for start_file in start_files
         m = match(r"start_(\d+)\.jld2$", start_file)
         run_idx = parse(Int, m.captures[1])
         # data = load(start_file, "data")
+
+        if !isnothing(selected_runs)
+            (run_idx in selected_runs) || continue
+        end
 
         # main(; run_name, save_data=true, data, run_idx)
         @info "Queuing run: problem:\"$(problem_name)\", run_name:\"$(run_name)\", run_idx:\"$(run_idx)\""
